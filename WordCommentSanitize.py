76116533cbb650514cf54ffb53cbb89fcf56d2ec
= <=>
2021-04-16 22:44:40 -0400

    Working version to remove comment dates

--- WordCommentSanitize.py
+++ WordCommentSanitize.py
@@ -1,67 +1,69 @@
-# This is adapted from the solution to StackOverflow question 37955062 about
-# removing personal information from comments in a .docx file, accessible at
-
-# https://stackoverflow.com/a/37956562
-
 import re
 import os
+import argparse
 from zipfile import ZipFile
 
-import re
-import os
-from zipfile import ZipFile
+class DocxZip:
 
+    def __init__(self, filename):
+        self.filename = filename
+        self.files = self.get_internal_files()
 
-docx_file_name = "document.docx"
+        if 'word/comments.xml' in self.files.keys():
+            self.simple_comments()
 
-files = dict()
-with ZipFile(docx_file_name, 'r') as document_as_zip:
-    for internal_file in document_as_zip.infolist():
-        file_reader = document_as_zip.open(internal_file.filename, "r")
-        files[internal_file.filename] = file_reader.readlines()
-        file_reader.close()
-        
-if "word/comments.xml" in files.keys():
-    # We will be working on comments file...
-    comments = files["word/comments.xml"]
+        if 'word/commentsExtensible.xml' in self.files.keys():
+            self.extensible_comments()
 
-    comments_new = str()
+        self.replace_file()
 
-    # Files contents have been read as list of byte strings.
-    for comment in comments:
-        if isinstance(comment, bytes):
-            # Change every author to "Unknown Author".
-            comments_new += re.sub(r'w:date="[^"]*"', '', comment.decode())
+    def get_internal_files(self):
+        files = dict()
 
-    files["word/comments.xml"] = comments_new
+        with ZipFile(self.filename, 'r') as document_as_zip:
+            for internal_file in document_as_zip.infolist():
+                with document_as_zip.open(internal_file.filename, 'r') as file_reader:
+                    files[internal_file.filename] = file_reader.readlines()
+        return files
+
+    def simple_comments(self):
+        comments = self.files['word/comments.xml']
+        comments_new = str()
+
+        for comment in comments:
+            if isinstance(comment, bytes):
+                comments_new += re.sub(r'w:date="[^"]*"', '', comment.decode())
+
+        self.files['word/comments.xml'] = comments_new
+
+    def extensible_comments(self):
+        comments_x = self.files['word/commentsExtensible.xml']
+        comments_x_new = str()
+
+        for comment in comments_x:
+            if isinstance(comment, bytes):
+                comments_x_new += re.sub(r'w16cex:dateUtc="[^"]*"', '', comment.decode())
+
+        self.files['word/commentsExtensible.xml'] = comments_x_new
+
+    def replace_file(self):
+        with ZipFile('sanitized'+self.filename, 'w') as document_as_zip:
+            for internal_file_name in self.files.keys():
+                merged_binary_data = str()
+                for binary_data in self.files[internal_file_name]:
+                    if not isinstance(binary_data, str):
+                        binary_data = binary_data.decode()
+
+                    merged_binary_data += binary_data
+
+                document_as_zip.writestr(internal_file_name, merged_binary_data)
     
-if "word/commentsExtensible.xml" in files.keys():
-    # We will be working on comments file...
-    comments = files["word/commentsExtensible.xml"]
+# os.remove(docx_file_name)
 
-    comments_new = str()
-
-    # Files contents have been read as list of byte strings.
-    for comment in comments:
-        if isinstance(comment, bytes):
-            # Change every author to "Unknown Author".
-            comments_new += re.sub(r'w16cex:dateUtc="[^"]*"', '', comment.decode())
-
-    files["word/commentsExtensible.xml"] = comments_new
-    
-os.remove(docx_file_name)
-
-with ZipFile(docx_file_name, 'w') as document_as_zip:
-    for internal_file_name in files.keys():
-        # Those are lists of byte strings, so we merge them...
-        merged_binary_data = str()
-        for binary_data in files[internal_file_name]:
-            # If the file was not edited (therefore is not the comments.xml file).
-            if not isinstance(binary_data, str):
-                binary_data = binary_data.decode()
-
-            # Merge file contents.
-            merged_binary_data += binary_data
-
-        # We write old file contents to new file in new .docx.
-        document_as_zip.writestr(internal_file_name, merged_binary_data)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=str)
+    args = parser.parse_args()
+    docx_filename = args.file
+    # docx_filename = 'original.docx'
+    new_file = DocxZip(docx_filename)
